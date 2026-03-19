ea23df3b0928bf5ba3e6cb7a13456404e94733c5
kannan <kannans89@hotmail.com>
2026-03-19 15:00:19 +0530

    'initila'

---
+++ CustomerDbSampleSecurityapp/CustomerDbSampleSecurityapp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        CaseStudy1();
+
+
+    }
+
+    private static void CaseStudy1()
+    {
+        string connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=companydb;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        try
+        {
+            connection.Open();
+            Console.WriteLine("connected successfully to " + connection.Database);
+            string customerId = "";
+            Console.WriteLine("Enter cusotmer id ");
+            customerId = Console.ReadLine();
+            String customerByIdQuery = "select * from customer where id=" + customerId;
+            SqlCommand cmd = new SqlCommand(customerByIdQuery, connection);
+            using (SqlDataReader dr = cmd.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    Console.WriteLine("{0} {1} {2} ", dr["id"], dr["name"], dr["balance"]);
+                }
+            }
+
+
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.StackTrace);
+        }
+        finally
+        {
+            if (connection != null && connection.State == ConnectionState.Open)
+                connection.Close();
+        }
+    }
+}
